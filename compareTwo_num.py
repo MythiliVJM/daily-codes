674a0987451b01213a7ef14e1b6b1ee200ffd1a3
Mythili <96191870+MythiliVJM@users.noreply.github.com>
2022-08-19 18:56:51 +0530

    Update compareTwo_num.py

--- compareTwo_num.py
+++ compareTwo_num.py
@@ -1,3 +1,12 @@
 a=23
 b=45
 print("a and b both are equal" if a==b else "a is greater than b" if a>b else "b greater than a")
+
+#model-2
+
+a=5
+b=7
+  
+# [statement_on_True] if [condition] else [statement_on_false] 
+  
+print(a,"is greater") if (a>b) else print(b,"is Greater")
